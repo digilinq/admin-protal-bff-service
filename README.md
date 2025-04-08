47624a524964c15fc8170bf79d86c30fcb157659
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2025-04-08 15:36:53 +0200

    Generate access token on successful authentication

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # admin-protal-bff-service
 This repository is a backend service for admin protal created by node powered by express framework
+
+
--- server.js
+++ server.js
@@ -1,128 +1,58 @@
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
-const fsPromises = require("fs").promises;
-const os = require("os");
-const logEvent = require("./logEvents");
-const EventEmitter = require("events");
-
-class Logger extends EventEmitter { 
-  log(...args) {
-    console.log(...args);
-    this.emit("log", ...args);
-  }
-};
-
-const logger = new Logger();
-
-logger.on("log", (...args) => {
-  logEvent("log.txt", ...args);
-});
-
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
 const PORT = process.env.PORT || 3500;
 
-const serveFile = async (filePath, contentType, response) => {
-  try {
-      const rawData = await fsPromises.readFile(
-          filePath,
-          !contentType.includes('image') ? 'utf8' : ''
-      );
-      const data = contentType === 'application/json'
-          ? JSON.parse(rawData) : rawData;
-      response.writeHead(
-          filePath.includes('404.html') ? 404 : 200,
-          { 'Content-Type': contentType }
-      );
-      response.end(
-          contentType === 'application/json' ? JSON.stringify(data) : data
-      );
-  } catch (err) {
-      console.log(err);
-      myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
-      response.statusCode = 500;
-      response.end();
-  }
-} 
+// custom middleware logger
+app.use(logger);
 
-const server = http.createServer((req, res) => {
-  logger.log(req.method, req.url);  
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
 
-  const extension = path.extname(req.url);
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
 
-  let contentType;
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
 
-  switch (extension) {
-      case '.css':
-          contentType = 'text/css';
-          break;
-      case '.js':
-          contentType = 'text/javascript';
-          break;
-      case '.json':
-          contentType = 'application/json';
-          break;
-      case '.jpg':
-          contentType = 'image/jpeg';
-          break;
-      case '.png':
-          contentType = 'image/png';
-          break;
-      case '.txt':
-          contentType = 'text/plain';
-          break;
-      default:
-          contentType = 'text/html';
-  }
+// built-in middleware for json 
+app.use(express.json());
 
-  let filePath =
-      contentType === 'text/html' && req.url === '/'
-          ? path.join(__dirname, 'views', 'index.html')
-          : contentType === 'text/html' && req.url.slice(-1) === '/'
-              ? path.join(__dirname, 'views', req.url, 'index.html')
-              : contentType === 'text/html'
-                  ? path.join(__dirname, 'views', req.url)
-                  : path.join(__dirname, req.url);
+//middleware for cookies
+app.use(cookieParser());
 
-  // makes .html extension not required in the browser
-  if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
 
-  const fileExists = fs.existsSync(filePath);
+// routes
+app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
 
-  if (fileExists) {
-      serveFile(filePath, contentType, res);
-  } else {
-      switch (path.parse(filePath).base) {
-          case 'old-page.html':
-              res.writeHead(301, { 'Location': '/new-page.html' });
-              res.end();
-              break;
-          case 'www-page.html':
-              res.writeHead(301, { 'Location': '/' });
-              res.end();
-              break;
-          default:
-              serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
-      }
-  }
+app.use(verifyJWT);
+app.use('/employees', require('./routes/api/employees'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 });
 
-server.listen(PORT, () => {
-  logger.log(`Server is running on port ${PORT}`);
-});
+app.use(errorHandler);
 
-
-// setTimeout(() => {
-//   logger.log("OS Type:", os.type());
-//   logger.log("OS Version:", os.version());
-//   logger.log("Home Directory:", os.homedir());
-//   logger.log("OS Platform:", os.platform());
-//   logger.log("OS Architecture:", os.arch());
-//   logger.log("OS Release:", os.release());
-//   logger.log("OS Hostname:", os.hostname());
-//   logger.log("OS Uptime:", os.uptime(), "seconds");
-// }, 2000);
-
-
-
-
-
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
