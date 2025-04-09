78d05b05334bf341057eaa28fd2fda3eea823bc8
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2025-04-09 17:42:15 +0200

    Add authentication and refresh token handler

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 This repository is a backend service for admin protal created by node powered by express framework
 
 
+```shell
+curl -D - -X POST http://localhost:3500/auth \
+  -H "Content-Type: application/json" \
+  -d '{"username": "walt1", "password": "Aa$12345"}'
+```
+
+```shell
+curl -D - http://localhost:3500/employees -H "Content-Type: application/json" -H "Authorization: Bearer YOUR_TOKEN_HERE" 
+```
+
+We will receive a new access token by hitting the /referesh endpoint
+```shell 
+curl -D - -b "PUT_THE_COOKIE_HERE" http://localhost:3500/refresh 
+```
--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,9 +1,17 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+const isValidBearerToken = (token) => {
+    if (!token) return false;
+    const tokenParts = token.split(' ');
+    if (tokenParts.length !== 2) return false;
+    if (tokenParts[0] !== 'Bearer') return false;
+    return true;
+}
+
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    if (!authHeader) return res.sendStatus(401);
+    if (!isValidBearerToken(authHeader)) return res.sendStatus(401);
     console.log(authHeader); // Bearer token
     const token = authHeader.split(' ')[1];
     jwt.verify(
